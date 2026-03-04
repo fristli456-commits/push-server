692baf5575fb0cd22e8f6bc70b5cc71bc39c6fdc
fristli456-commits <fristli456@gmail.com>
2026-03-04 18:22:49 +0300

    add apk

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static("public"));
 // 🔥 Заменим на твой ключ
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
