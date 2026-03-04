db6e2318ad7d5a4470e9d0b4bd045ca0d6c11ef9
fristli456-commits <fristli456@gmail.com>
2026-03-04 21:22:33 +0300

    upload apk

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ app.post("/sendPush", async (req, res) => {
   }
 });
 
-app.listen(5000, () =>
-  console.log("Push server running on port 5000")
-);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Push server running");
+});
